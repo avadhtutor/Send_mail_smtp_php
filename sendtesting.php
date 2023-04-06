ecb1b79f8de7e60ec4066e7fed56d3d20e0a4eb9
ripal123 <pandyaripal@gmail.com>
2023-04-06 20:57:11 +0530

    Send_mail_smtp_php
    send smtp mail using php example

---
+++ sendtesting.php
@@ -0,0 +1,30 @@
+<?php
+  require_once('phpmailer/class.phpmailer.php');
+	define('GUSER', 'username');	 
+	define('GPWD', 'password');
+	function smtpmailer($to, $from, $from_name, $subject, $body) { 
+	global $error;
+	$mail = new PHPMailer();  // create a new object
+	$mail->IsSMTP(); // enable SMTP
+	$mail->SMTPDebug = 0;  // debugging: 1 = errors and messages, 2 = messages only
+	$mail->SMTPAuth = true;  // authentication enabled
+	$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
+	$mail->Host = 'smtp.gmail.com';
+	$mail->Port = 465; 
+	$mail->Username = '';  
+	$mail->Password = '';           
+	$mail->SetFrom($from, $from_name);
+	$mail->Subject = $subject;
+	$mail->Body = $body;
+	$mail->AddAddress($to);
+	if(!$mail->Send()) {
+		$error = 'Mail error: '.$mail->ErrorInfo; 
+		return false;
+	} else {
+		$error = 'Message sent!';
+		return true;
+	}
+}
+if(smtpmailer('pandyaripal@gmail.com','dharat2@gmail.com', 'Subject Msg Here', 'Heading Here', 'message Goes here...'))
+		echo $error;
+?>
